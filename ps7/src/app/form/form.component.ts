ae69976ce3e0f1080376435ba97b2506b6eca1f0
Justin Moy <jkmoy@bu.edu>
2019-12-08 12:20:50 -0500

    done

--- ps7/src/app/form/form.component.ts
+++ ps7/src/app/form/form.component.ts
@@ -35,11 +35,11 @@ export class FormComponent implements OnInit {
     // const headers = new HttpHeaders().set('Access-Control-Allow-Methods', '*')
     // const params = new HttpParams().set('Access-Control-Allow-Methods', '*')
     // for testing w/o having to start backend + mongo servers
-    const url = 'https://jsonplaceholder.typicode.com';
+    // const url = 'https://jsonplaceholder.typicode.com';
     // this.data = this.http.get(url + '/posts?userId=1');
     // this.data = this.http.get(url + '/posts/1/');
     // this.data = this.http.get('https://pokeapi.co/api/v2/pokemon/bulbasaur/');
-    this.data = this.http.get('http://localhost:3000/ps6/');
+    this.data = this.http.get('http://localhost:3000/ps6/' + name);
     this.display = true;
     console.log('==============================');
     console.log(this.data);
